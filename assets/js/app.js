bfc071cd25abf922c8ddf6401d1f2123a4027351
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-05-01 18:32:41 -0700

    edits

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const close = document.querySelector(".close");
   const header = document.querySelector("header");
   const mobileLinks = document.querySelectorAll("aside a");
+  const part1 = document.querySelector(".part_1");
+  const part2 = document.querySelector(".part_2");
+  const readMore = document.querySelector(".read_more");
+  const readLess = document.querySelector(".read_less");
+
+  part2.style.display = "none";
+
+  readMore.addEventListener("click", () => {
+    if (part2.style.display === "none") {
+      part2.style.display = "block";
+      part1.style.display = "none";
+    }
+  });
+
+  readLess.addEventListener("click", () => {
+    if (part1.style.display === "none") {
+      part1.style.display = "block";
+      part2.style.display = "none";
+    }
+  });
 
   const links = Array.from(mobileLinks);
 
